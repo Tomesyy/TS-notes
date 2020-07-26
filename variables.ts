140b67e4ed51fb5ef66b89504c3a150a5b131db3
tomesyy <adetomiwaadesanya01@gmail.com>
2020-07-26 20:33:02 +0100

    array&tuples declaration notes

--- variables.ts
+++ variables.ts
@@ -8,9 +8,13 @@ let winner: boolean = true;
 let sum: number = age + age;
 
 // ARRAY
+// FORMAT: let arrayName: type[] = [];
 let names: string[] = [];
 names.push("John")
 names.push("Tomiwa")
 names.push("BlackDev")
 
+// TUPLE - is a special structure in TS that can hold as many
+// -values as needed
+// FORMAT: let tupleName: [type, type, type] = [];
 let person: [string, number, boolean] = ["Tobi", 19, true];
