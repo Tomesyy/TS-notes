b3127e042ff083767abc65a483c82a8b3e8d277e
tomesyy <adetomiwaadesanya01@gmail.com>
2020-07-26 20:28:16 +0100

    variable declaration notes

---
+++ variables.ts
@@ -0,0 +1,16 @@
+// variable declarations 
+// FORMAT: let variable: type = value;
+
+let myname: string = "Tomiwa";
+let age: number = 10;
+let winner: boolean = true;
+
+let sum: number = age + age;
+
+// ARRAY
+let names: string[] = [];
+names.push("John")
+names.push("Tomiwa")
+names.push("BlackDev")
+
+let person: [string, number, boolean] = ["Tobi", 19, true];
