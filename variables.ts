27d684525e32a40c273c24ce8978670344975390
tomesyy <adetomiwaadesanya01@gmail.com>
2020-07-26 21:27:37 +0100

    interface declaration notes

--- variables.ts
+++ variables.ts
@@ -47,3 +47,55 @@ class Person{
     }
 }
 let person1 = new Person("Adetomiwa", "Adesanya");
+
+//INTERFACES
+// interfaces are just similar to struct in solidity
+// FORMAT
+/* interface interfaceName{
+*    property: type;
+*  }
+*/
+// interfaces define the contract that other classes 
+// or objects must comply with if implementing that interface
+
+interface PersonInterface{
+    name: string;
+    age: number;
+    good: boolean;
+}
+
+function infor(person: PersonInterface): number{
+    return person.age;
+}
+
+class Student implements PersonInterface{
+    name: string;
+    age: number;
+    good: boolean;
+    constructor(a: string, b: number, c: boolean){
+        this.name = a;
+        this.age = b;
+        this.good = c;
+    }
+}
+
+// EXAMPLE
+interface Panthera {
+    roar: string;
+}
+
+class Lion implements Panthera {
+    roar: string;
+    constructor(){
+        this.roar = 'roaarrrrr';
+    }
+}
+
+class Tiger implements Panthera {
+    roar: string;
+    constructor(){
+        this.roar = 'ROAARRRRRRR'
+    }
+}
+//Tiger and Lion implement the panthera interface, which mean
+// Tiger and Lion must have a roar property
